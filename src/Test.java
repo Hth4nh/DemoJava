9cd1958c83ca1c5ec1d4742ce8af65ce51231264
hth4nh <cronshina@gmail.com>
2024-01-14 14:14:57 +0700

    First commit.

---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        int n = (int)Math.ceil(Math.pow(Math.PI, 2));
+        System.out.println(n);
+
+        System.out.println("Just testing :))");
+    }
+}
