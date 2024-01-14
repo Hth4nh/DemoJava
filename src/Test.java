6b758bca418310978a62ce9904a6d212df808b03
hth4nh <cronshina@gmail.com>
2024-01-14 15:29:08 +0700

    VSCode GUI commit test (again)

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        int n = (int)Math.ceil(Math.pow(Math.PI, 3));
+        int n = (int)Math.ceil(Math.pow(Math.PI, 4));
         System.out.println(n);
 
         System.out.println("Just testing :))");
