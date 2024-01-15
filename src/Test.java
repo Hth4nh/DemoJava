e6501221b092f58d76b7d932d3fa564a57e21252
Thành Hoàng Trần <hth4nh@gmail.com>
2024-01-15 11:54:24 +0700

    GitHub.dev commit test

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        int n = (int)Math.ceil(Math.pow(Math.PI, 4));
+        int n = (int)Math.ceil(Math.pow(Math.PI, 5));
         System.out.println(n);
 
         System.out.println("Just testing :))");
